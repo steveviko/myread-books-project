b1765db3dfda82eaeb282e447d47d36d3f1571d6
steven opio <stevenviko@gmail.com>
2021-06-26 13:33:06 +0300

    Add Book component to shelf

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Book from './Book';
 
 class Shelf extends Component {
     state = {  }
@@ -10,34 +11,24 @@ class Shelf extends Component {
         return ( 
 
             <div className="bookshelf">
-        <h2 className="bookshelf-title">{this.props.title}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-          {shelfBooks.map( book => (
-            <li key={book.id}>
-              <div className="book">
-                <div className="book-top">
-                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                  <div className="book-shelf-changer">
-                    <select value={book.shelf} onChange={e => this.props.changeShelf(book, e.target.value)}>
-                      <option value="move" disabled>Move to...</option>
-                      <option value="currentlyReading">Currently Reading</option>
-                      <option value="wantToRead">Want to Read</option>
-                      <option value="read">Read</option>
-                      <option value="none">None</option>
-                    </select>
-                  </div>
-                </div>
-                <div className="book-title">{book.title}</div>
-                <div className="book-authors">{book.authors}</div>
-              </div>
-            </li>
+                <h2 className="bookshelf-title">{this.props.title}</h2>
+                <div className="bookshelf-books">
+                <ol className="books-grid">
+                    {this.props.books.filter(book => book.shelf === 'currentlyReading').map( book => (
+                        <li key={book.id}>
+                        <Book
+                            book={book}
+                            changeShelf= {this.props.changeShelf}
+                            assignedShelf={book.shelf}
 
-          ))}
-          </ol>
-        </div>
+                        />
+                        </li>
+
+                    ))}
+                </ol>
+                </div>
       </div>
-      
+
          );
     }
 }
