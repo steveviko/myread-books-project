87f0c9a724433288a1558d141f3ba6f4e7d9b45b
steven opio <stevenviko@gmail.com>
2021-06-26 16:58:42 +0300

    Add builds for deployment

---
+++ build/precache-manifest.298df45fa2019ed5d032111dd727ed83.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "868c00d0d3511fb21bd6",
+    "url": "/static/css/main.b1c5b05f.chunk.css"
+  },
+  {
+    "revision": "868c00d0d3511fb21bd6",
+    "url": "/static/js/main.868c00d0.chunk.js"
+  },
+  {
+    "revision": "fbd81e831cb8ef58db86",
+    "url": "/static/js/1.fbd81e83.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "4ebbffcc62c17b7b529342b2347a73c5",
+    "url": "/index.html"
+  }
+];
