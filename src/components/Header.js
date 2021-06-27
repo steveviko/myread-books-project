29fcefa4458a41ee24fe2caf6e30a11568f69329
steven opio <stevenviko@gmail.com>
2021-06-27 17:00:00 +0300

    Modify header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import MenuIcon from '@material-ui/icons/Menu';
+import { Link } from 'react-router-dom';
+import '../Header.css';
 
 class Header extends Component {
     state = {  }
     render() { 
         return (
             <div className="list-books-title">
-              <h1>MyReads</h1>
+                 
+                 <div className="header">
+                     <div className="logo">
+                    < MenuIcon className="menu_icon"/>
+                    <Link to="/"  className="link">                    
+                    <span className="title"> MyReads</span>
+                    </Link>
+                    </div>
+                 </div>
+            
+              
             </div>
         );
     }
